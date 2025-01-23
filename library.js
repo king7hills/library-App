a619878968270479fb1c81deab231f63b088d33e
king7hills <haasjh1@gmail.com>
2025-01-23 11:44:56 -0500

    Added delete buttons. Wrapped diplay of books in a function.

--- library.js
+++ library.js
@@ -1,29 +1,67 @@
 const myLibrary = [];
 
-function Book(title, author, read_status) {
+function Book(title, author, read_status, id) {
     this.title = title;
     this.author = author;
     this.read_status = read_status;
+    this.id = id;
 };
+
+const book1 = new Book('Extreme Ownership', 'Jocko Willink', 'I have read it!', myLibrary.length);
+myLibrary.push(book1);
+
+const book2 = new Book('Thinking Fast and Slow', 'Daniel Khanneman', 'I have not read it.', myLibrary.length);
+myLibrary.push(book2);
 
 const libraryDisplay = document.querySelector('div.library_display');
 
+// Add existing books to display
+function displayBooks () {
+    myLibrary.forEach((book) => {
+    const p = document.createElement('p');
+    p.setAttribute('data-book-id', book.id);
+    p.classList.add("book_entry");
 
-const book1 = new Book('Extreme Ownership', 'Jocko Willink', 'read');
-myLibrary.push(book1);
+    let text = '';
+    for (let x in book) {
+        text += x + ': ' + book[x] + "<br>";
+    };
 
-function addLibraryBook (title, author, read_status) {
-    let freshbook = new Book(title, author, read_status);
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Remove Book';
+    deleteButton.setAttribute('data-book-id', book.id);
+    deleteButton.onclick = function (event) {
+        deleteBook(event);
+    }
+
+    p.innerHTML = text;
+    p.appendChild(deleteButton);
+    libraryDisplay.appendChild(p);
+})};
+
+function addLibraryBook (title, author, read_status, id) {
+    let freshbook = new Book(title, author, read_status, id);
     myLibrary.push(freshbook);
 
     //Must create new element EACH TIME function is run
     const p = document.createElement('p');
-    libraryDisplay.appendChild(p);
+    p.setAttribute('data-book-id', book.id);
+    p.classList.add("book_entry");
     let text = '';
     for (let x in freshbook) {
-        text += x + ': ' + freshbook[x] + " ";
+        text += x + ': ' + freshbook[x] + "<br>";
     };
-    p.textContent = text;
+   
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Remove Book';
+    deleteButton.setAttribute('data-book-id', book.id);
+    deleteButton.onclick = function (event) {
+        deleteBook(event);
+    }
+
+    p.innerHTML = text;
+    p.appendChild(deleteButton);
+    libraryDisplay.appendChild(p);
 };
 
 const dialog = document.querySelector('dialog');
@@ -46,17 +84,17 @@ form.addEventListener("submit", (e) => {
     let newTitle = formStuff.get('Title');
     let newAuthor = formStuff.get('Author');
     let newStatus = formStuff.get('Read Yet?');
+    let id = myLibrary.length;
 
     if (newStatus == 'on') {
         newStatus = 'I read it!';
     } else newStatus = 'I have not read it.';
         
-    addLibraryBook(newTitle, newAuthor, newStatus);
+    addLibraryBook(newTitle, newAuthor, newStatus, id);
     dialog.close();
     form.reset();
 });
 
-
-// let newTitle = document.querySelector('input#title');
-//     let newAuthor = document.querySelector('input#author');
-//     let newStatus = document.querySelector('input#read_status');
+function removeDOM () {
+    
+}
